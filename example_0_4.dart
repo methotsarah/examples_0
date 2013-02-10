b67172641ebad5ca92471b5bef912a4bc45998c2
Sarah Méthot <methot.sarah@gmail.com>
2013-02-10 12:47:53 -0800

    Create example_0_4.dart

---
+++ example_0_4.dart
@@ -0,0 +1,29 @@
+main(){
+  var x = 0;
+  var y = 1;
+  print (x);
+  print (y);
+  x = x + y;
+  print(x);
+  y = x + y;
+  print(y);
+  x = x + y;
+  print(x);
+  y = x + y;
+  print(y);
+  x = x + y;
+  print(x);
+  y = x + y;
+  print(y);
+  x = x + y;
+  print(x);
+  y = x + y;
+  print(y);
+  x = x + y;
+  print(x);
+  y = x + y;
+  print(y);
+  x = x + y;
+  print(x);
+}
+
